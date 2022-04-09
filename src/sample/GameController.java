16686980cd214cf4d7b9c77696c27bc0c5a2b729
anagalacticRuby <nick12.aded@gmail.com>
2022-04-09 00:18:49 -0400

    Version 1.7 - Added comments to GameController.class - Spruced up with more JDoc comments - Reformatted code to clean up

--- src/sample/GameController.java
+++ src/sample/GameController.java
@@ -1,15 +1,27 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
-
 import java.util.Random;
 
+/**
+ * The controller class for the Rock Paper Scissors program
+ *
+ * @author anagalacticRuby
+ */
 public class GameController {
 
+  // A private variable that keeps count of how many games the player has lost
   private int gamesLostCount;
+
+  // A private variable that keeps count of how many games the player has tied
   private int gamesTiedCount;
+
+  // A private variable that keeps count of how many games the player has won
   private int gamesWonCount;
 
   @FXML // fx:id="playerRockButton"
@@ -57,16 +69,45 @@ public class GameController {
   @FXML // fx:id="winnerLabel"
   private Label winnerLabel; // Value injected by FXMLLoader
 
+  /**
+   * This method is tied to the 'Battle!' button, and will trigger whenever the button is pressed.
+   *
+   * @param event An event listener that awaits the button being clicked
+   */
   @FXML
   void playRockPaperScissors(MouseEvent event) {
     setComputerChoice();
+    // First, the computer decides what weapon it will select.
+    // NOTE: The computer does NOT look at your input, in order to maintain a fair game.
     String gameResult = determineWinner();
+    // A string is set to the value returned by determineWinner() for quick setting of the results.
+
     winnerLabel.setText(gameResult);
+    /*
+     * Once a result has been obtained, update the label in the center of the screen to display
+     * the outcome obtained by determineWinner (now currently being stored in gameResult.
+     */
+
     updateScorecard(gameResult);
+    // Next, update the scorecard that rests at the top of the screen using the string stored in
+    // gameResult.
   }
 
+  /**
+   * This method will check the selected options of both players and determine a winner.
+   *
+   * <p>After a winner is determined, the counter for the respective result is incremented by one.
+   *
+   * @return A string message of the game result is returned
+   */
   private String determineWinner() {
 
+    /*
+     * Large conditional if-else chains are used here because the game needs to check for two things.
+     * The player's choice, and the computer's choice.
+     * After these two things have been obtained,
+     * they are then compared to one another to determine a winner of the game.
+     */
     if ((playerRockButton.isSelected() && computerRockButton.isSelected())
         || (playerPaperButton.isSelected() && computerPaperButton.isSelected())
         || (playerScissorsButton.isSelected() && computerScissorsButton.isSelected())) {
@@ -90,6 +131,11 @@ public class GameController {
     }
   }
 
+  /**
+   * This method allows for the computer to have a 'fair' chance of picking any option equally.
+   *
+   * <p>There are only three possible options to choose from, but only one may be picked.
+   */
   private void setComputerChoice() {
     Random randomGenerator = new Random();
     int randomNumber = randomGenerator.nextInt(3);
@@ -102,37 +148,73 @@ public class GameController {
     }
   }
 
+  /**
+   * This method is to be run after determineWinner(), so that accurate scores are kept.
+   *
+   * <p>A string message is accepted as a parameter for quick and easy comparisons.
+   *
+   * @param gameResult A string containing the result of the most recent game that was played.
+   */
   private void updateScorecard(String gameResult) {
     if (gameResult.equals("Tied Game")) {
       tiedGamesLabel.setText(getGamesTiedCount() + " Ties");
     } else if (gameResult.equals("Player Wins")) {
       gamesWonLabel.setText(getGamesWonCount() + " Wins");
     } else {
-
       lostGamesLabel.setText(getGamesLostCount() + " Losses");
     }
   }
 
+  /**
+   * Obtains the current count of games that the player has lost
+   *
+   * @return returns the count of games lost by the player as an integer
+   */
   public int getGamesLostCount() {
     return gamesLostCount;
   }
 
+  /**
+   * Mutator that sets the value of the counter for games lost by the player
+   *
+   * @param gamesLostCount An integer value of games lost
+   */
   public void setGamesLostCount(int gamesLostCount) {
     this.gamesLostCount = gamesLostCount;
   }
 
+  /**
+   * Accessor to obtain the count of games that ended in a tie
+   *
+   * @return returns the count of games ending in a tie
+   */
   public int getGamesTiedCount() {
     return gamesTiedCount;
   }
 
+  /**
+   * Mutator that sets the value of the counter for games that ended in a tie
+   *
+   * @param gamesTiedCount An integer value of the games ending in a tie
+   */
   public void setGamesTiedCount(int gamesTiedCount) {
     this.gamesTiedCount = gamesTiedCount;
   }
 
+  /**
+   * Accessor to obtain the count of games won by the player
+   *
+   * @return returns the count of games won by the player as an integer
+   */
   public int getGamesWonCount() {
     return gamesWonCount;
   }
 
+  /**
+   * Mutator that sets the value of the counter for games won by the player
+   *
+   * @param gamesWonCount An integer value of the games won by the player
+   */
   public void setGamesWonCount(int gamesWonCount) {
     this.gamesWonCount = gamesWonCount;
   }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,11 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The main driver class for RPS
+ * @author anagalacticRuby
+ */
 public class Main extends Application {
 
+  /**
+   * The starting method of the program.
+   *
+   * <p> This method will run upon execution of the program and loads the stage for fx elements.</p>
+   *
+   * @param primaryStage The initial stage. There only needs to be one!
+   * @throws Exception The method will throw an IllegalStateException if the stage is shown on another thread
+   */
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
     primaryStage.setTitle("Hello World");
     primaryStage.setScene(new Scene(root, 700, 300));
     primaryStage.show();
